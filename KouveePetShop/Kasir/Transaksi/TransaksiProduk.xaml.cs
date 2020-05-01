3d2e95f74e63037c0400163570613f5717760fdf
Marvel <taniaberskill@gmail.com>
2020-05-01 13:51:26 +0700

    tampil transaksi produk done

--- KouveePetShop/Kasir/Transaksi/TransaksiProduk.xaml.cs
+++ KouveePetShop/Kasir/Transaksi/TransaksiProduk.xaml.cs
@@ -37,6 +37,9 @@ namespace KouveePetShop
                 connection = "Server=localhost; User Id=root;Password=;Database=petshop;Allow Zero Datetime=True";
                 conn = new MySqlConnection(connection);
                 conn.Open();
+                SubTotal();
+                Diskon();
+                Total();
                 TampilDataGrid();
                 conn.Close();
             }
@@ -49,8 +52,12 @@ namespace KouveePetShop
         private void TampilDataGrid()
         {
             // Tampil data ke dataGrid
-            MySqlCommand cmd = new MySqlCommand("select tr.NO_TRANSAKSI, h.NAMA_HEWAN, tr.DISKON, dt.TOTAL, tr.STATUS_PEMBAYARAN, tr.ID_PEGAWAI, tr.ID_PEGAWAI2 from transaksi tr JOIN detail_transaksi_produk dt ON tr.ID_TRANSAKSI = dt.ID_TRANSAKSI " +
-                "JOIN hewan h on tr.ID_HEWAN = h.ID_HEWAN where tr.NO_TRANSAKSI LIKE 'PR%'", conn);
+            MySqlCommand cmd = new MySqlCommand("select tr.NO_TRANSAKSI, h.NAMA_HEWAN, tr.DISKON, dt.TOTAL, tr.STATUS_PEMBAYARAN, tr.ID_PEGAWAI, pg1.NAMA_PEGAWAI as nama1, pg2.NAMA_PEGAWAI as nama2, tr.ID_PEGAWAI2 from transaksi tr " +
+                "JOIN detail_transaksi_produk dt ON tr.ID_TRANSAKSI = dt.ID_TRANSAKSI " +
+                "JOIN hewan h on tr.ID_HEWAN = h.ID_HEWAN J" +
+                "OIN pegawai pg1 on tr.ID_PEGAWAI = pg1.ID_PEGAWAI " +
+                "JOIN pegawai pg2 on tr.ID_PEGAWAI2 = pg2.ID_PEGAWAI " +
+                "where tr.NO_TRANSAKSI LIKE 'PR%'", conn);
             try
             {
                 //conn.Open();
@@ -65,18 +72,52 @@ namespace KouveePetShop
             }
         }
 
-        private void Diskon()
+        private void SubTotal()
         {
             try
             {
                 using(MySqlCommand cmd = new MySqlCommand())
-                {
-                    conn.Open();
+                {                  
                     cmd.Connection = conn;
-                    cmd.CommandText = "Update detail_transaksi_produk dt set SUB_TOTAL = (dt.DISKON * (dt.JUMLAH * p.HARGA_PRODUK) / 100) join produk p where dt.ID_PRODUK = p.ID_PRODUK ";
+                    cmd.CommandText = "UPDATE detail_transaksi_produk dt SET dt.SUB_TOTAL = (SELECT sum(dt.JUMLAH*p.HARGA_PRODUK) FROM produk p WHERE dt.ID_PRODUK = p.ID_PRODUK)";
+                    cmd.ExecuteNonQuery();
                 }
             }
             catch(Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private void Diskon()
+        {
+            try
+            {
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "UPDATE transaksi tr SET tr.DISKON = (SELECT sum(dt.SUB_TOTAL * (10/100)) FROM detail_transaksi_produk dt WHERE tr.ID_TRANSAKSI = dt.ID_TRANSAKSI)";
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private void Total()
+        {
+            try
+            {
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "UPDATE detail_transaksi_produk dt SET TOTAL = ( SELECT sum(dt.SUB_TOTAL - tr.DISKON) FROM transaksi tr WHERE dt.ID_TRANSAKSI = tr.ID_TRANSAKSI)";
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception err)
             {
                 MessageBox.Show(err.Message);
             }
